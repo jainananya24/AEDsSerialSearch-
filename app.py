1c4521423ca697e4dcdaa7fb9be09d8e5b5eb74a
jainananya24 <ananya@avive.life>
2025-04-10 11:15:03 -0700

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import gspread
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaIoBaseDownload
